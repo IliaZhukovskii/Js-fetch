02782be8248bddd56a411664a042fcde50af0e88
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-29 13:25:16 +0300

    fetch

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 'use strict';
 
+const getData = () => {
 
+  const user = fetch('./db.json');
+  user
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      sendDate('https://jsonplaceholder.typicode.com/posts', data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+getData();
+
+const sendDate = (link, object) => {
+  fetch(link, {
+    method: 'POST',
+    body: JSON.stringify(object),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+};
